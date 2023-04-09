a2f37098b0e1462b4574df370b0b13d6b5ea24f0
Mohan7659 <129480563+Mohan7659@users.noreply.github.com>
2023-04-09 22:46:28 +0530

    Add files via upload

---
+++ programme Python.py
@@ -0,0 +1,33 @@
+# x='python'
+# x=5
+# x=6.4
+# print(x)
+# print(x)
+# print(x)
+
+import keyword as k
+print(k.iskeyword('while'))
+print(k.kwlist)
+print(len(k.kwlist))
+
+# x, y, z = "orange","banana","cherry"
+# print(x)
+# print(y)
+# print(z)
+
+name=input()
+age=int(input())
+qualification=input()
+percentage=float(input())
+print(f'your name is {name}, your age is {age}, your qualification is {qualification}, your perccentage is {percentage}')
+
+# a=5
+# b=7.8
+# print(a)
+# print(b)
+
+# print(type(a))
+# print(type(b))
+
+
+
